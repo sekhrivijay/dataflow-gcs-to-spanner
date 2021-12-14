bb0350d0595479fb73ed339d2744c45a7bb82389
Vijay Sekhri <sekhrivijay@google.com>
2021-12-14 22:31:04 +0000

    redact

--- setup_pubsub_avro_to_postgres.sh
+++ setup_pubsub_avro_to_postgres.sh
@@ -2,7 +2,7 @@ export PROJECT=sekhrivijayrcs1
 export BUCKET_NAME="gs://sekhrivijay-rcs-dataflow-test1"
 export TEMP_LOCATION="${BUCKET_NAME}/temp"
 export POSTGRES_USER=postgres
-export POSTGRES_PASSWORD=CnnaoB5i0bO4ml5t
+export POSTGRES_PASSWORD=XXXXXXXXXXXXXXXXXXXXXXXXXXX
 
 export POSTGRES_DATABASE_INSTANCE="sekhrivijayrcs1:us-central1:rcs-instance"
 export POSTGRES_DATABASE_NAME=mytestdb
